d442c5860f5530923996d5e051b273561b6f46e4
MRDC-Leinnn <mrdelacruz0916@gmail.com>
2026-02-20 16:44:57 +0800

    Update Activity 4.py

--- Laboratory 1/Activity 4.py
+++ Laboratory 1/Activity 4.py
@@ -27,4 +27,5 @@ print(f"{x}{x}. {x}: {x}{x}: {x}{x}{x}{x}{x}{x}{x}{x}{x}: {x}{x}.. {x}{x}::: {x}
 print(f"{x}{x}:.:: {x}{x}: {x}{x}.... {x}{x}: {x}{x}::. {x}{x}:: {x}{x}::: {x}{x}:")
 print(f"{x}{x}:::: {x}{x}: {x}{x}:::: {x}{x}: {x}{x}:::. {x}{x}:. {x}{x}{x}{x}{x}{x}::")
 
-# Para maging dynamic ASCII Art, papalitan yung main character (in this case, '#'), to the variable name {f-string} kung saan naka-assign yung user input (in this case, x)
+# Para maging dynamic ASCII Art, papalitan yung main character (in this case, '#'), to the variable name using f-string kung saan naka-assign yung user input (in this case, x)
+# In essence print("#") -> f"{x}"      x being the variable name nung user input
