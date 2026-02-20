1c9b504ae349ece8e0769433d40af3006f86c035
MRDC-Leinnn <mrdelacruz0916@gmail.com>
2026-02-20 16:09:54 +0800

    Delete Activity 3.py

--- Activity 3.py
+++
@@ -1,9 +0,0 @@
-# ACTIVITY 3: USER INPUT
-
-#Asks for the user to input
-name = input("Name: ")
-age = input("Age: ")
-hobby = input("Favorite Hobby: ")
-
-# Displays the user input in a sentence format
-print(f"You are {name}, you are {age} years old, and your favorite hobby is {hobby}.")
