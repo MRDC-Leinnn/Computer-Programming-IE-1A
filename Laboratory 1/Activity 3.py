01ae13dfa35eef8551c5c2d366208f6083accf36
MRDC-Leinnn <mrdelacruz0916@gmail.com>
2026-02-20 16:17:07 +0800

    Add files via upload

---
+++ Laboratory 1/Activity 3.py
@@ -0,0 +1,9 @@
+# ACTIVITY 3: USER INPUT
+
+#Asks for the user to input
+name = input("Name: ")
+age = input("Age: ")
+hobby = input("Favorite Hobby: ")
+
+# Displays the user input in a sentence format
+print(f"You are {name}, you are {age} years old, and your favorite hobby is {hobby}.")
