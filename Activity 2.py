a32e38ea4cdd4b33ba169398960fa6b6df7779ae
MRDC-Leinnn <mrdelacruz0916@gmail.com>
2026-02-20 16:07:43 +0800

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,15 @@
+# ACTIVITY 2: DATA TYPES
+
+# Sample variables of different data types
+string = "Marc"
+number =  25
+decimal = 25.2
+bool = True
+list = ["Hilarie", "Joesper", "Richford", "Roniel", "Anthony"]
+
+# type(variable_name) is used to check the data type of a variable
+print("Name:", string, "- Data Type:", type(string))
+print("Number:", number, "- Data Type:", type(number))
+print("Decimal:", decimal, "- Data Type:", type(decimal))
+print("Boolean:", bool, "- Data Type:", type(bool))
+print("Students:", list, "- Data Type:", type(list))
